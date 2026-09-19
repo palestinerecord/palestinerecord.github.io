36a4da56975a5c68ae68bac3dc2d604a8ec3a4d5
palestinerecord <328386359+palestinerecord@users.noreply.github.com>
2026-09-20 00:25:23 +0100

    Record the UK ministerial statements, the Palantir contract record and the Board of Peace membership

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@
    this worker and empties every cache it made.
    ============================================================ */
 
-const VERSION = 'v107';
+const VERSION = 'v108';
 const SHELL = 'record-shell-' + VERSION;
 const DATA = 'record-data';
 const MINE = /^record-(shell|data)/;
@@ -42,15 +42,15 @@ const MINE = /^record-(shell|data)/;
 const SHELL_FILES = [
   './',
   './index.html',
-  './css/style.css?v=107',
-  './js/charts.js?v=107',
-  './js/share.js?v=107',
-  './js/views.js?v=107',
-  './js/app.js?v=107',
-  './js/scene.js?v=107',
+  './css/style.css?v=108',
+  './js/charts.js?v=108',
+  './js/share.js?v=108',
+  './js/views.js?v=108',
+  './js/app.js?v=108',
+  './js/scene.js?v=108',
   './manifest.webmanifest',
-  './assets/flag-palestine.svg?v=107',
-  './assets/favicon.svg?v=107',
+  './assets/flag-palestine.svg?v=108',
+  './assets/favicon.svg?v=108',
   './assets/icon-192.png',
   './assets/icon-512.png',
 ];
